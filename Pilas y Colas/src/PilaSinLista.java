0b83e175728b6b936409537c86de69ca3790aae1
Andrés Torres <atorres@medialima.mx>
2020-03-03 07:49:31 -0600

    Pilas

---
+++ Pilas y Colas/src/PilaSinLista.java
@@ -0,0 +1,21 @@
+public class PilaSinLista<T> {
+
+    private Nodo<T> inicial;
+
+    public void push(T Elemento){
+
+    }
+
+    public T peek(){
+
+    }
+
+    public T pop(){
+
+    }
+
+    public boolean estaVacia(){
+        
+    }
+
+}
