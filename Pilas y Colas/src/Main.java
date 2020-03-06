b0597bed89574a4a686b3ad2522184c8c9fb62ec
Andrés Torres <atorres@medialima.mx>
2020-03-06 08:20:48 -0600

    Pilas y Colas

--- Pilas y Colas/src/Main.java
+++ Pilas y Colas/src/Main.java
@@ -2,5 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Pila<Integer> pila= new Pila<>();
+        pila.push(4);
+        pila.push(5);
+        pila.pop();
+
     }
 }
