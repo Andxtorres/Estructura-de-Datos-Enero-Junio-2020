47d7f4c046e81210830d2fdee6c09012f7ac79a1
Andrés Torres <atorres@medialima.mx>
2020-03-27 08:28:57 -0600

    Ordenamientos

--- Ordenamientos/src/Main.java
+++ Ordenamientos/src/Main.java
@@ -3,23 +3,28 @@ public class Main {
     public static void main(String[] args) {
 
         ListaLigada<Integer> lista= new ListaLigada<>();
+        lista.insertarAlUltimo(15);
+        lista.insertarAlUltimo(14);
+        lista.insertarAlUltimo(13);
+        lista.insertarAlUltimo(12);
+        lista.insertarAlUltimo(11);
         lista.insertarAlUltimo(10);
-        lista.insertarAlUltimo(3);
-        lista.insertarAlUltimo(2);
-        lista.insertarAlUltimo(7);
-        lista.insertarAlUltimo(8);
-        lista.insertarAlUltimo(1);
         lista.insertarAlUltimo(9);
+        lista.insertarAlUltimo(8);
+        lista.insertarAlUltimo(7);
+        lista.insertarAlUltimo(6);
         lista.insertarAlUltimo(5);
         lista.insertarAlUltimo(4);
-        lista.insertarAlUltimo(6);
-        lista.insertarAlUltimo(16);
-        lista.insertarAlUltimo(24);
+        lista.insertarAlUltimo(3);
+        lista.insertarAlUltimo(2);
+        lista.insertarAlUltimo(1);
+
+
 
         lista.imprimeLista();
         System.out.println("-------------------------------");
         Ordenamiento<Integer> ordenamiento= new Ordenamiento();
-        ordenamiento.bubbleSort(lista);
+        ordenamiento.insertionSort(lista);
         lista.imprimeLista();
 
 
--- Ordenamientos/src/Ordenamiento.java
+++ Ordenamientos/src/Ordenamiento.java
@@ -21,4 +21,44 @@ public class Ordenamiento<T extends Comparable<T>> {
 
         }
     }
+
+    public void selectionSort(ListaLigada<T> lista){
+        int contador=0;
+        for(int i=0;i<lista.contarElementos()-1;i++){
+            int smallest=i;
+            for(int j=i+1;j<lista.contarElementos()-1;j++){
+                if(lista.obtenerElElementoEnLaPos(j).compareTo(lista.obtenerElElementoEnLaPos(smallest))<0){
+                    System.out.println("El más pequeño es: "+lista.obtenerElElementoEnLaPos(j));
+                    smallest=j;
+                }
+                contador++;
+            }
+            System.out.println("Intercambiando "+lista.obtenerElElementoEnLaPos(i)+" con "+lista.obtenerElElementoEnLaPos(smallest));
+            lista.intercambia(i,smallest);
+            lista.imprimeLista();
+            contador++;
+        }
+        System.out.println("Contador"+contador);
+
+    }
+
+    public void insertionSort(ListaLigada<T> lista){
+
+        int i=1;
+        int contador=0;
+        while(i<lista.contarElementos()){
+            int j=i;
+            while(j>0 && (lista.obtenerElElementoEnLaPos(j-1).compareTo(lista.obtenerElElementoEnLaPos(j))>0)){
+                System.out.println("Intercambia "+lista.obtenerElElementoEnLaPos(j)+" con "+lista.obtenerElElementoEnLaPos(j-1));
+                lista.intercambia(j,j-1);
+                lista.imprimeLista();
+                j--;
+                contador++;
+            }
+            i++;
+            contador++;
+        }
+
+        System.out.println("Contador: "+contador);
+    }
 }
